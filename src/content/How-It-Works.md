b06d2244847527348b3fc89eed9dc9d89186fb2b
Shujaagideon <60461755+Shujaagideon@users.noreply.github.com>
2020-08-19 15:25:27 +0300

    post number 2

---
+++ src/content/How-It-Works.md
@@ -0,0 +1,47 @@
+---
+layout: post
+title: How a computer works 1
+image: img/computer.jpg
+author: [The Shujaa]
+date: 2020-07-18T10:00:00.000Z
+tags:
+  - Simple
+excerpt: The simplest example that explains how a computer works
+draft: false
+---
+
+## __Who doesn't like Food__
+
+Just the other day in the restaurant with a good friend the waiter approached and asked what I would like for lunch well am not really good at choosing so I Went with good old noodles with some beef stew and some pepper to taste.
+
+> Noodles really ?!  was there no better choice!!
+
+I know. I am so bad at choosing but anyway, that aside, what went on during the conversation is the simplest key to understanding basically how a computer works. So what happened? Lets break this into three simple segments:
+ - The question the waiter asked.
+ - The time I took thinking of the best response.
+ - The response it self.
+
+Let us try to define what a computer is. This is a device that takes input processes the input and gives back an output relevant to the input.
+
+
+> so what about all those noodles
+
+Back to the noodles. So when the waiter was asking me what food I was going to eat, he was basically giving me a set of instructions, which is the input, which in much simpler terms he was asking my brain to think of which food I would prefer eating. Then I took My time to Think of the food I was going to eat. This is the processing time in a computer. finally I gave him a relevant response which is equivalent to output.
+
+>So a computer imitates the human mind?
+
+You can say so. For example, when you press the key _"A"_ on the keyboard of your phone or computer, you are essentially trying to tell the computer to print the letter _"A"_ on the screen so the information is taken to the brain of the computer where the information is processed and finally you see the letter _"A"_ on the screen.
+
+>Well if you guessed that the keyboard is an input device and the screen monitor an output device you guessed it right.
+
+ Okay I wont bore you with any more question but the next intriguing question that you should ask your self is how does computer know that I am pressing the key _"A"_ and not another key? and how does that information move to the brain of the computer? In fact what is the brain of the computer?
+
+
+ In the next part we will talk about the hardware the tangible part of the computer.
+
+
+
+
+
+
+
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -57,43 +57,17 @@ const About: React.FC = () => (
             <PostFullContent className="post-full-content">
               <div className="post-content">
                 <h2>
-                  The Shujaa <br /> GitHub: <a href="https://github.com/scttcper/gatsby-casper">scttcper/gatsby-casper</a>
+                  The Shujaa <br /> GitHub: <a href="https://github.com/scttcper/gatsby-casper">The Shujaa</a>
                 </h2>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo finibus leo,
-                  non tempus magna vehicula ac. Maecenas mollis ante finibus pharetra imperdiet.
-                  Maecenas in aliquam purus. Nam et massa a nulla fermentum dapibus sit amet in
-                  neque. Ut ipsum ipsum, rhoncus a sodales pellentesque, interdum a elit. Nullam
-                  aliquam tellus nibh, eget laoreet dui aliquet non. Vestibulum malesuada ante at
-                  diam tempus, ac interdum risus scelerisque. Sed ipsum neque, vulputate porta diam
-                  eget, consequat blandit nulla. Integer volutpat velit vitae purus lacinia aliquam.
-                  Integer bibendum ipsum vitae magna pulvinar, nec vehicula dolor vulputate. Nulla
-                  eu massa id orci placerat finibus vel sit amet eros. Vestibulum quis consequat
-                  massa. Sed sagittis sollicitudin massa at commodo. Praesent diam nisi, imperdiet
-                  posuere eleifend nec, blandit ac massa.
+                 My name is Shujaa a software developer still a student in the school of learning,
+                 has a high affinity for information and a very dedicated tech enthusiast.
+                 I have quite an experience in web development after working on some very magnificent
+                 projects, using some great tools along the way, that have delivered an intriguing user
+                 experience. You can find some of my works on my portfolio at <a href="http://The-Shujaa.com">The Shujaa </a>
+                along with some of my other information
                 </p>
-                <p>
-                  Vestibulum semper pretium ipsum nec congue. Ut ac eros nisi. Donec leo sem,
-                  aliquam mollis sapien ultrices, dapibus congue diam. Proin viverra dapibus
-                  blandit. Ut mauris tellus, tristique id felis vel, venenatis vestibulum nunc. Nam
-                  molestie pulvinar nibh, eget egestas augue. Maecenas tellus arcu, mattis ut ipsum
-                  non, sollicitudin convallis nunc. Donec nec neque tristique, aliquet lacus id,
-                  laoreet nunc. Cras dapibus nisi nulla, ullamcorper faucibus neque suscipit ac.
-                  Donec eget orci venenatis justo lobortis volutpat. Proin vel placerat nisl.
-                  Integer arcu nunc, sodales eu fringilla non, aliquam non diam. Cras placerat,
-                  massa et faucibus pretium, ante elit tincidunt tellus, tristique ultricies velit
-                  quam et massa.
-                </p>
-                <p>
-                  In nunc lacus, dapibus vitae lacus sit amet, efficitur iaculis neque. Suspendisse
-                  ut tellus quis leo vestibulum tincidunt. Aenean nec enim ac dolor lacinia semper.
-                  Ut sed laoreet libero. Nunc elementum sollicitudin accumsan. Nunc eu augue neque.
-                  Proin a tortor nibh. Cras eu nisl ornare sapien feugiat pellentesque. Mauris
-                  dignissim vel quam eu pellentesque. Integer sit amet posuere quam, eu ullamcorper
-                  odio. Nullam a lacus tempus sapien dignissim ullamcorper. In hac habitasse platea
-                  dictumst. Proin quis massa aliquam, feugiat tortor sit amet, tincidunt urna. Donec
-                  posuere pulvinar lectus, ac semper ipsum vulputate quis.
-                </p>
+               
               </div>
             </PostFullContent>
           </article>
